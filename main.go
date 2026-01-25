63cd42e75468898b3f599eb9733383b5f9174cec
TimurVR <138249026+TimurVR@users.noreply.github.com>
2026-01-25 10:23:35 +0300

    The firt task

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+func main(){
+ 	r := gin.Default()
+	r.Use(gin.Recovery())
+ 	r.GET("/ping", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+    })
+  	if err := r.Run(":8080"); err != nil {
+    	log.Fatalf("failed to run server: %v", err)
+		panic("failed to run server")
+  	}
+}
