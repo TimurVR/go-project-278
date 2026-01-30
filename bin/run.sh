5ff9c9daa90b9ec2e9d2396fd98deffad8c6324a
TimurVR <138249026+TimurVR@users.noreply.github.com>
2026-01-30 10:18:53 +0300

    The fix seventh task

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+export DATABASE_URL="postgresql://go_project_user:5crLwQD0QYVCjkppX5Dtjn2IPWvoBz5@dpg-d5svobu3jp1c738v4g40-a.oregon-postgres.render.com/go_project_db?sslmode=require"
 echo "[run.sh] Starting service"
 
 echo "[run.sh] Running DB migrations"
