2dcd919ec076e273ddc9222cd30a75d55f140ebb
TimurVR <138249026+TimurVR@users.noreply.github.com>
2026-01-30 17:08:36 +0300

    The fix seventh task

--- bin/run.sh
+++ bin/run.sh
@@ -7,7 +7,7 @@ echo "[run.sh] Running DB migrations"
 goose -dir ./db/migrations postgres "${DATABASE_URL}" up
 
 echo "[run.sh] Starting Caddy"
-caddy run --config /etc/caddy/Caddyfile &
+caddy start --config /etc/caddy/Caddyfile
 
 echo "[run.sh] Starting Go app"
 exec /app/bin/app
