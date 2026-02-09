84f17b9bfb1f891394ff244d5dd26d54ade76b12
TimurVR <138249026+TimurVR@users.noreply.github.com>
2026-02-09 22:55:54 +0300

    temp

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-export DATABASE_URL="postgresql://go_project_user:Fj2SbLdlar3a4l48bXHObp5r6ewZEzpO@dpg-d5u8jbh4tr6s739dbca0-a/go_project_db_h0do"
+export DATABASE_URL
 echo "[run.sh] Starting service"
 
 echo "[run.sh] Running DB migrations"
